d20cfb7decbdb15e1dd1e9b30530609af851d750
wei <1604335421@qq.com>
2020-09-11 19:07:03 +0800

    完成index页面

---
+++ assets/js/index.js
@@ -0,0 +1,43 @@
+$(function(){
+    getUserInfo()
+    
+})
+function getUserInfo(){
+    $.ajax({
+        type:'GET',
+        url:'/my/userinfo',
+        success:function(res){
+        if(res.status!==0){
+          return layui.layer.msg('获取用户信息失败！')
+        }
+         return  renderAvatar(res.data)
+        },
+    //    complete:function(res){
+    //      console.log(res);
+    //      if(res.responseJSON.status===1&&res.responseJSON.message==='身份认证失败！'){
+    //         localStorage.removeItem('token')
+    //         location.href = '/login.html'
+    //      }
+    //    }
+    })
+}
+function  renderAvatar(user){
+    var name=user.nickname||user.username
+    $('#welcome').html('欢迎&nbsp;&nbsp;'+name)
+    if(user.user_pic!==null){
+        $('.layui-nav-img').attr('src',user.user_pic).show()
+        $('.text-avatar').hide()
+    }else{
+        $('.layui-nav-img').hide()
+        var first=name[0].toUpperCase()
+        $('.text-avatar').html(first).show()
+    }
+}
+var layer = layui.layer
+$('#btnLogout').on('click', function() {
+    layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function(index) {
+      localStorage.removeItem('token')
+      location.href = '/login.html'
+      layer.close(index)
+    })
+  })
